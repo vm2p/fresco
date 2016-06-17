003935be448568605e2ed2725833026e46d787d9
Peter Sebastian Nordholt <peter.s.nordholt@alexandra.dk>
2016-06-17 14:57:42 +0200

    added the equality protocol

--- src/main/java/dk/alexandra/fresco/lib/newcompare/NewCompareFactoryImpl.java
+++ src/main/java/dk/alexandra/fresco/lib/newcompare/NewCompareFactoryImpl.java
@@ -4,13 +4,27 @@ import dk.alexandra.fresco.framework.value.SInt;
 import dk.alexandra.fresco.lib.compare.eq.EqualityProtocol;
 import dk.alexandra.fresco.lib.compare.gt.GreaterThanProtocol;
 import dk.alexandra.fresco.lib.compare.zerotest.ZeroTestProtocol;
+import dk.alexandra.fresco.lib.field.integer.BasicNumericFactory;
+import dk.alexandra.fresco.lib.math.integer.PreprocessedNumericBitFactory;
+import dk.alexandra.fresco.lib.math.integer.exp.ExpFactory;
+import dk.alexandra.fresco.lib.math.integer.exp.ExpFromOIntFactory;
+import dk.alexandra.fresco.lib.newcompare.subprotocols.ComparisonSubProtocolFactory;
+import dk.alexandra.fresco.lib.newcompare.subprotocols.ComparisonSubProtocolFactoryImpl;
 
 public class NewCompareFactoryImpl implements NewCompareFactory {
+	
+	private ComparisonSubProtocolFactory subFac;
+	private BasicNumericFactory bnFac;
+
+	public NewCompareFactoryImpl(BasicNumericFactory bnFac, ExpFactory expFac, PreprocessedNumericBitFactory pnbFac) {
+		this.subFac = new ComparisonSubProtocolFactoryImpl(bnFac, expFac, pnbFac);
+		this.bnFac = bnFac;
+	}
 
 	@Override
 	public ZeroTestProtocol getZeroTestProtocol(SInt x, SInt result, int bitLength, int secParam) {
-		// TODO Auto-generated method stub
-		return null;
+		ZeroTestProtocol ztp = new NewZeroTestProtocol(x, result, bitLength, secParam, subFac, bnFac);
+		return ztp;
 	}
 
 	@Override
@@ -21,8 +35,8 @@ public class NewCompareFactoryImpl implements NewCompareFactory {
 
 	@Override
 	public EqualityProtocol getEqualityProtocol(SInt x, SInt y, SInt result, int bitLength, int secParam) {
-		// TODO Auto-generated method stub
-		return null;
+		EqualityProtocol eqp = new NewEquality(x, y, result, secParam, bitLength, bnFac, this);
+		return eqp;
 	}
 
 }
